a37a96ad4de3a9dccf4b24bf123d29dd9f774060
aliciusschroeder <alicius.schroeder@gmail.com>
2024-10-14 21:41:44 +0000

    Create hook to subscribe to mediaQuery changes

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+export function useMediaQuery(query: string) {
+  const subscribe = React.useCallback(
+    (callback: { (this: MediaQueryList, ev: MediaQueryListEvent): any; (this: MediaQueryList, ev: MediaQueryListEvent): any; }) => {
+      const matchMedia = window.matchMedia(query);
+
+      matchMedia.addEventListener("change", callback);
+      return () => {
+        matchMedia.removeEventListener("change", callback);
+      };
+    },
+    [query]
+  );
+
+  const getSnapshot = () => {
+    return window.matchMedia(query).matches;
+  };
+
+  return React.useSyncExternalStore(subscribe, getSnapshot);
+}
