e095e1e47fb760ec7dd68e48eb0640f7e7aed88b
aliciusschroeder <alicius.schroeder@gmail.com>
2024-10-13 19:14:07 -0500

    Add cost-estimation

---
+++ src/lib/costEstimation.ts
@@ -0,0 +1,20 @@
+import type { GenerationOptions } from "@/types";
+
+const costPerImage = (settings: GenerationOptions) => {
+  if (settings.hdQuality) {
+    if (settings.aspectRatio === "square") {
+      return 0.08;
+    } else {
+      return 0.12;
+    }
+  } else {
+    if (settings.aspectRatio === "square") {
+      return 0.04;
+    } else {
+      return 0.08;
+    }
+  }
+}
+
+export const estimateCost = (settings: GenerationOptions) => 
+  costPerImage(settings) * settings.numImages;
