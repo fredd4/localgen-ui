21b91d04f9d750d9769a4f3d484847c430cc7c42
aliciusschroeder <alicius.schroeder@gmail.com>
2024-10-15 00:49:03 +0000

    Add documentation for Utility Functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,9 +2,26 @@ import { clsx, type ClassValue } from "clsx";
 import { PrimitiveAtom, atom } from 'jotai';
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Provides a utility function to construct className strings conditionally.
+ * 
+ * It leverages clsx and tailwind-merge for flexibility and consistency.
+ * clsx handles conditional class inclusion, and tailwind-merge merges Tailwind classes effectively.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * Creates a derived atom that accesses and updates a specific field within an object-like atom.
+ * 
+ * This is useful for creating individual atoms for properties of a larger object stored in a base atom.
+ * It allows you to work with specific fields directly without needing to update the entire object.
+ * 
+ * @param baseAtom The base atom holding the object-like state.
+ * @param field The key of the field you want to derive an atom for.
+ * @returns A new atom that represents the specified field within the base atom.
+ */
 export function createFieldAtom<T extends object, K extends keyof T>(
   baseAtom: PrimitiveAtom<T>,
   field: K
