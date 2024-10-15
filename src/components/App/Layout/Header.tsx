32b120c49b56747ef0d628d3ac04c1617f5c8148
aliciusschroeder <alicius.schroeder@gmail.com>
2024-10-15 00:37:32 +0000

    Add settings popover to Header component
    - Introduce Settings popover in the header
    - Implement responsive layout for header elements

--- src/components/App/Layout/Header.tsx
+++ src/components/App/Layout/Header.tsx
@@ -1,14 +1,32 @@
+import Settings from "@/components/App/Settings/Settings";
+import { Button } from "@/components/ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { motion } from "framer-motion";
+import { Settings2Icon } from "lucide-react";
 
 const Header = () => (
   <header className="mb-8">
-    <motion.h1
-      initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <span className="bg-clip-text text-5xl font-bold">DALL-E Web App</span>
-    </motion.h1>
+    <div className="flex items-center justify-between">
+      <motion.h1
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <span className="bg-clip-text text-5xl font-bold">DALL-E Web App</span>
+      </motion.h1>
+      <div className="flex items-center justify-center ml-4">
+        <Popover modal>
+          <PopoverTrigger asChild>
+            <Button size="icon" className="bg-secondary text-secondary-foreground border-border hover:bg-muted focus:ring-ring disabled:bg-muted disabled:text-muted-foreground">
+              <Settings2Icon />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-96" side="left" style={{ boxShadow: "0 0 0 12000px rgba(0, 0, 0, 0.32)", }} collisionPadding={{ top: 32 }}>
+            <Settings />
+          </PopoverContent>
+        </Popover>
+      </div>
+    </div>
   </header>
 );
 
