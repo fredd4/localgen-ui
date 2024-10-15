221f4729028a248e1a39de2f68325b43eddef747
aliciusschroeder <alicius.schroeder@gmail.com>
2024-10-15 00:53:05 +0000

    Add Documentation

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,10 +1,15 @@
 import * as React from "react";
 
+/**
+ * Hook that utilizes `useSyncExternalStore` to subscribe to media query changes.
+ *
+ * @param query - The media query string to listen for.
+ * @returns A boolean indicating whether the media query currently matches.
+ */
 export function useMediaQuery(query: string) {
   const subscribe = React.useCallback(
     (callback: (event: MediaQueryListEvent) => void) => {
       const matchMedia = window.matchMedia(query);
-
       matchMedia.addEventListener("change", callback);
       return () => {
         matchMedia.removeEventListener("change", callback);
