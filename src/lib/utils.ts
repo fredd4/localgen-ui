6cf080dc7c6fb18e416c3f0e89b9b2c8ab1dfc69
aliciusschroeder <alicius.schroeder@gmail.com>
2024-10-15 00:48:47 +0000

    Add utility function for Jotai field atoms
    - Implement createFieldAtom for granular state management

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
 import { clsx, type ClassValue } from "clsx";
+import { PrimitiveAtom, atom } from 'jotai';
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+export function createFieldAtom<T extends object, K extends keyof T>(
+  baseAtom: PrimitiveAtom<T>,
+  field: K
+) {
+  return atom(
+    // Getter: Reads the value of the specified field from the base atom.
+    (get) => get(baseAtom)[field],
+    (get, set, newValue: T[K]) => {
+      const currentObject = get(baseAtom);
+      // Setter: Updates the base atom by creating a new object with the field updated.
+      set(baseAtom, { ...currentObject, [field]: newValue });
+    }
+  );
 }
