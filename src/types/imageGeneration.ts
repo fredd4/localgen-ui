4a53335b572b863f41ae983e580fac0d9f13d4ff
aliciusschroeder <alicius.schroeder@gmail.com>
2024-10-15 00:41:55 +0000

    Extend GenerationOptions interface
    - Add prompt and useExactPrompt fields to GenerationOptions

--- src/types/imageGeneration.ts
+++ src/types/imageGeneration.ts
@@ -4,6 +4,8 @@ export type AspectRatioValue = AspectRatioSetting["value"];
 export type StyleValue = "natural" | "vivid";
 
 export interface GenerationOptions {
+  prompt: string;
+  useExactPrompt: boolean;
   aspectRatio: AspectRatioValue;
   style: StyleValue;
   hdQuality: boolean;
