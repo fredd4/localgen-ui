f7bd41eca0374f51b97429469d0be6c9871d7dd5
aliciusschroeder <alicius.schroeder@gmail.com>
2024-10-17 13:21:20 +0000

    Display all locally saved images

--- src/components/App/History/History.tsx
+++ src/components/App/History/History.tsx
@@ -1,23 +1,31 @@
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-} from "@/components/ui/card";
+import ImageCard from "@/components/ImageCard";
+import { Card, CardContent } from "@/components/ui/card";
+import { useManageSavedImages } from "@/hooks/useManageSavedImages";
+import { savedImagesAtom } from "@/store/atoms";
+import { useAtomValue } from "jotai";
 
-const History = () => (
-  <Card>
-    <CardHeader>
-      <CardTitle>Generation History</CardTitle>
-      <CardDescription>
-        View and manage your previous generations
-      </CardDescription>
-    </CardHeader>
-    <CardContent>
-      <p>History view coming soon...</p>
-    </CardContent>
-  </Card>
-);
+const History = () => {
+  const savedImages = useAtomValue(savedImagesAtom);
+  const loadSavedImages = useManageSavedImages();
+  loadSavedImages();
+
+  return (
+    <Card>
+      <CardContent className="mt-4">
+        {
+          savedImages.length === 0 ? (
+            <p>No images have been generated yet.</p>
+          ) : (
+            <div className="flex">
+              {savedImages.map((image) => (
+                <ImageCard key={image.id} generatedImage={image} />
+              ))}
+            </div>
+          )
+        }
+      </CardContent>
+    </Card>
+  );
+}
 
 export default History;
