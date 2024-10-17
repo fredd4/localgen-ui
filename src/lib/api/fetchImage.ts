8a49408c8b09fd6b509606bf60a0225c20c5eb76
aliciusschroeder <alicius.schroeder@gmail.com>
2024-10-17 13:23:34 +0000

    Add Api-Key as parameter to fetchImage

--- src/lib/api/fetchImage.ts
+++ src/lib/api/fetchImage.ts
@@ -5,7 +5,8 @@ function wait(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export const fetchImage = async (options: GenerationOptions) => {
+export const fetchImage = async (options: GenerationOptions, apiKey: string) => {
+  console.log("API Key is set: ", (apiKey !== ""));
   const url =
     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII";
   const cost = estimateCost({ ...options, numImages: 1 });
